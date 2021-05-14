6fac4d32f19c3b9c23fd6099b9adb987a54a411b
Ryan Helm <rjhelm@github.io>
2021-05-13 22:18:54 -0600

    work on home view

---
+++ jt-design/src/Components/Home/Home2.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I am an Artist from Salt Lake City, Utah. It started with doodling with my brother and turned
+              into a passion for creativity that I have used in my life ever since.
+              <br />
+              <br />I have artwork that includes
+              <i>
+                <b className="purple"> Digital, Drawing, Sketching, Painting </b>
+              </i>
+              <br />
+              <br />
+              I have a up and coming tatoo portfolio as well.  &nbsp;
+              <i>
+                <b className="purple">I enjoy concepts brought to me by others{" "}</b>
+                <b className="purple">
+                  or my own creations.
+                </b>
+              </i>
+              <br />
+              <br />
+              I have alot of artwork after years of practice and hard work
+              <i>
+                <b className="purple"> iPad drawings</b>
+              </i>
+              &nbsp; ||
+              <i>
+                <b className="purple"> Paintings</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in//"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com//"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Home2;
