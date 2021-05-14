202a76c62faa7902c11d5759fa2407f22c36d5b6
Ryan Helm <rjhelm@github.io>
2021-05-13 19:57:35 -0600

    added artwork for portfolio

--- jt-design/src/Components/Artwork.js
+++ jt-design/src/Components/Artwork.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import Thumbnail from './Thumbnail.js';
+import jclark from '../../public/img/jclark.jpg';
+import jid from '../../public/img/jid.jpg';
+import juju from '../../public/img/juju.jpg';
+import reach from '../../public/img/reach.jpg';
+import she from '../../public/img/she.jpg';
 
 function Artwork(props) {
     return (
         <div>
             <h1>Artwork</h1>
+            <Thumbnail
+                link="/jclark"
+                image={jclark}
+                title="Jordan Clarkson"
+                category="Digital"
+            />
+            <Thumbnail
+                link="/jid"
+                image={jid}
+                title="JID"
+                category="Digital"
+            />
+            <Thumbnail
+                link="/juju"
+                image={juju}
+                title="Juju the Avenger"
+                category="Digital"
+            />
+            <Thumbnail
+                link="/reach"
+                image={reach}
+                title="Untitled"
+                category="Digital"
+            />
+            <Thumbnail
+                link="/she"
+                image={she}
+                title="Bae?"
+                category="Digital"
+            />
         </div>
     )
 }
--- jt-design/src/Components/Thumbnail.js
+++ jt-design/src/Components/Thumbnail.js
@@ -1,52 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import jclark from '../../public/img/jclark.jpg';
-import jid from '../../public/img/jid.jpg';
-import juju from '../../public/img/juju.jpg';
-import reach from '../../public/img/reach.jpg';
-import she from '../../public/img/she.jpg';
 import './App.css';
 
 function Thumbnail(props) {
     return (
         <div className="artwork">
-            <Link to="">
+            <Link to={props.link}>
                 <div className="artwork-image">
-                    <img src={jclark} alt='Jordan Clarkson Artwork' />
+                    <img src={props.image} alt="Artwork by JT" />
                 </div>
-                <div className="artwork-title">Jordan Clarkson</div>
-                <div className="artwork-category">Created on iPad</div>
-            </Link>
-            <Link to="">
-                <div className="artwork-image">
-                    <img src={jid} alt='JID Artwork' />
-                </div>
-                <div className="artwork-title">JID</div>
-                <div className="artwork-category">Created on iPad</div>
-            </Link>
-            <Link to="">
-                <div className="artwork-image">
-                    <img src={juju} alt='JuJu The Avenger Artwork' />
-                </div>
-                <div className="artwork-title">The Avenger</div>
-                <div className="artwork-category">Created on iPad</div>
-            </Link>
-            <Link to="">
-                <div className="artwork-image">
-                    <img src={reach} alt='Hand of God Artwork' />
-                </div>
-                <div className="artwork-title">Untitled</div>
-                <div className="artwork-category">Created on iPad</div>
-            </Link>
-            <Link to="">
-                <div className="artwork-image">
-                    <img src={she} alt='Girl Artwork' />
-                </div>
-                <div className="artwork-title">Bae</div>
-                <div className="artwork-category">Created on iPad</div>
+                <div className="artwork-title">{props.title}</div>
+                <div className="artwork-category">{props.category}</div>
             </Link>
         </div>
-        
     )
 }
 
