a934a6ef1b8630a25763faa7aa29e8c83d334311
Ryan Helm <rjhelm@github.io>
2021-05-13 20:58:36 -0600

    changes to the navbar

--- jt-design/src/Components/Navbar.js
+++ jt-design/src/Components/Navbar.js
@@ -1,17 +1,104 @@
-import React from 'react';
-import logo from "../../public/img/logo.png"
+import React, { useState } from "react";
+import logo from "../../public/img/logo.png";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import { CgGitFork } from "react-icons/cg";
+import {
+  AiFillStar,
+  AiOutlineHome,
+  AiOutlineFundProjectionScreen,
+  AiOutlineUser,
+} from "react-icons/ai";
+import { CgFileDocument } from "react-icons/cg";
 
-function Navbar(props) {
-    return (
-        <Navbar bg="dark" variant="dark" sticky="top" className="navbar">
-            <img src={logo} className="logo" alt="JT-Design Logo" />
-            <nav className="mr-auto">
-                <a href="/about" className="nav-list">About</a>
-                <a href="/artwork" className="nav-list">Artwork</a>
-                <a href="/contact" className="nav-list">Contact</a>
-            </nav>
-        </Navbar>
-    )
+function NavBar() {
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavBar] = useState(false);
+  function scrollHandler() {
+    if (window.scrollY >= 20) {
+      updateNavBar(true);
+    } else {
+      updateNavBar(false);
+    }
+  }
+
+  window.addEventListener("scroll", scrollHandler);
+
+  return (
+    <Navbar
+      expanded={expand}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
+    >
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={logo} className="img-fluid logo" alt="JT-Design Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={() => {
+            updateExpanded(expand ? false : "expanded");
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="m1-auto" defaultActiveKey="#home">
+                <Nav.Item>
+                    <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+                        <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                    </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link s={Link}
+                to="/about"
+                onClick={() => updateExpanded(false)}>
+                    <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+                </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/project"
+                onClick={() => updateExpanded(false)}
+              >
+                <AiOutlineFundProjectionScreen
+                  style={{ marginBottom: "2px" }}
+                />{" "}
+                Artwork
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                as={Link}
+                to="/resume"
+                onClick={() => updateExpanded(false)}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </Nav.Link>
+            </Nav.Item>
+
+            <Nav.Item className="fork-btn">
+              <Button
+                href="https://github.com/soumyajit4419/Portfolio"
+                target="_blank"
+                className="fork-btn-inner"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                <AiFillStar style={{ fontSize: "1.1em" }} />
+              </Button>
+            </Nav.Item>
+            </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Navbar;
